7ed2cb0397a766ff43e541971eac689bd40fc407
Dhiaa Al-Shalabi <dhiaa.alshalabi@gmail.com>
2022-09-30 11:36:25 +0300

    Resolve build error for no-unused-vars.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import Meme from './components/Meme';
+// import Meme from './components/Meme';
 import Box from './components/Box';
 import './App.css'
 import boxes from "./boxes"
