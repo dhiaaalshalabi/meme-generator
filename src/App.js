744b214cd5f0d0784aca27506f2ae70ebfed808e
Dhiaa Al-Shalabi <dhiaa.alshalabi@gmail.com>
2022-10-03 09:45:52 +0300

    Uses of form component in app.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from './components/Header'
-import Meme from './components/Meme';
+// import Meme from './components/Meme';
 import './App.css'
+import Form from './components/Form'
 
 function App() {
   return (
     <div>
       <Header />
-      <Meme />
+      {/* <Meme /> */}
+      <Form />
     </div>
   )
 }
