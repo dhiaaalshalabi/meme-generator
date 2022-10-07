d2d183b7769d6588123ccfa9efaf974af98204e0
Dhiaa Al-Shalabi <dhiaa.alshalabi@gmail.com>
2022-10-07 08:58:09 +0300

    Revert the use of header and meme in app.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react'
-// import Header from './components/Header'
-import Signup from './components/Signup'
-// import Meme from './components/Meme';
+import Header from './components/Header'
+import Meme from './components/Meme';
 import './App.css'
-// import Form from './components/Form'
 
 function App() {
   return (
     <div>
-      {/* <Header /> */}
-      {/* <Meme /> */}
-      {/* <Form /> */}
-      <Signup />
+      <Header />
+      <Meme />
     </div>
   )
 }
